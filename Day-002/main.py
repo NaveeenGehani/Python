1a515c6ee68dcf3a143cb5463a31087adb06446e
Naveen Gehani <142261796+NaveeenGehani@users.noreply.github.com>
2025-07-26 16:23:58 +0000

    Add day-002 project

---
+++ Day-002/main.py
@@ -0,0 +1,19 @@
+# Day-002 Project
+
+userName = input("What is your name? ")
+userFavFood = input("What is your favorite food? ")
+userFavMusic = input("What is your favorite music? ")
+userFavPlace = input("What is your favorite place? ")
+
+print("Getting to know you!")
+print()
+print("You are")
+print(userName)
+print("You're probably hungry for")
+print(userFavFood)
+print("and you're definitely getting your groove on to")
+print(userFavMusic)
+print("living in the amazing")
+print(userFavPlace)
+print()
+print("It was great knowing you, Good Luck!")
